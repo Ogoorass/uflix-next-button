0bc59769a0d9ffb05dbc4c7ead3c0961a0ec3f80
Ogoorass <igor.piontek@wp.pl>
2024-02-24 17:50:08 +0100

    working dropdown menu

--- uflix-next-button.js
+++ uflix-next-button.js
@@ -102,6 +102,70 @@ function getSeasonIdFromDivId(divId) {
     return numberToConventionNumber(divId.replace("season", ""));
 }
 
+function switchDropdown(dropdown) {
+    dropdown.style.display === "block" ? dropdown.style.display = "none" : dropdown.style.display = "block";
+}
+
+
+function changeButtonStyle(button) {
+    if (button.style.borderRadius === dropdownButtonStyle.borderRadius) {
+        button.style.borderRadius = "8px 8px 0 0";
+        button.style.padding = "0px 8px";
+    } else {
+        button.style.borderRadius = dropdownButtonStyle.borderRadius;
+        button.style.padding = "1px 8px";
+    };
+}
+
+// ########################## STYLES ################################
+
+const dropdownButtonStyle = {
+    display: "inline-block",
+    padding: "1px 8px",
+    textAlign: "left",
+    backgroundColor: "#111113",
+    border: "1px solid #ffffff",
+    borderRadius: "8px",
+    color: "#707080",
+    width: "fit-content",
+    minHeight: "25px", 
+};
+
+const dropdownStyle = {
+    backgroundColor: "#111113",
+    color: "#707080",
+    fontFamily: "Inter, sans-serif",
+    borderColor: "#707080",
+    borderRadius: "10px",
+    borderWidth: "1px",
+    padding: "3px",
+    position: "relative",
+    display: "inline-block",
+};
+
+const dropdownContentStyle = {
+    position: "absolute",
+    top: "25px",
+    zIndex: "1",
+    display: "none",
+    backgroundColor: "#111113",
+    color: "#707080",
+    minWidth: "200px",
+    border: dropdownButtonStyle.border, // dropdown border same as button border
+    borderRadius: "0 8px 8px 8px",
+    overflow: "scroll",
+    maxHeight: "60vh",
+    
+};
+
+const dropdownLabelStyle = {
+    marginRight: "10px",
+};
+
+//TODO reszte przerobić tak jak tu i w 190 linijce
+
+// ######################### END STYLES ##############################
+
 function main() {
     // top element, that shows current episode
     const breadcrumbName = document.querySelector("li.breadcrumb-item.active");
@@ -116,31 +180,77 @@ function main() {
     // reduced link: https://uflix.cc/episode/house-2004/
     let linkWithoutSeasonAndEpisode = removeSeasonAndEpisode(document.URL);
 
+
+
+    // container for dropdown
+    const dropdown = document.createElement("div");
+    dropdown.id = "dropdown";
+    Object.entries(dropdownStyle).forEach(([key, val]) => {
+        dropdown.style[key] = val;
+    });
+    
+
     // list of episodes
-    const dropdown = document.createElement("select");
-    dropdown.name = "Episodes";
-    dropdown.style.backgroundColor = "#111113";
-    dropdown.style.color = "#707080";
-    dropdown.style.fontFamily = "Inter, sans-serif";
-    dropdown.style.borderColor = "#707080";
-    dropdown.style.borderRadius = "10px";
-    dropdown.style.borderWidth = "1px";
-    dropdown.style.padding = "3px";
-    dropdown.style.paddingLeft = "10px";
+    const dropdownContent = document.createElement("div");
+    dropdownContent.id = "dropdown-content";
+    Object.entries(dropdownContentStyle).forEach(([key, val]) => {
+        dropdownContent.style[key] = val;
+    });
+    
 
     // lebel for dropdown
-    const dropdownLabel = document.createElement("label");
-    dropdownLabel.htmlFor = "Episodes";
-    dropdownLabel.style.marginRight = "10px";
+    const dropdownLabel = document.createElement("span");
     const dropdownLabelText = document.createTextNode(`S${currentSeason}E:`);
+    Object.entries(dropdownLabelStyle).forEach(([key, val]) => {
+        dropdownLabel.style[key] = val;
+    });
 
-    /*
-            <label>`dropdownLabelText`</label>
-            <select>...</select>
-        */
+    // button for dropdown
+    const dropdownButton = document.createElement("button");
+    dropdownButton.id = "dropdown-button";
+    // set all style properties
+    Object.entries(dropdownButtonStyle).forEach(([key, val]) => {
+        dropdownButton.style[key] = val;
+    })
+    
+
+    dropdownButton.onclick = () => { 
+        switchDropdown(dropdownContent); 
+        changeButtonStyle(dropdownButton); 
+        const topPos = document.getElementById(currentSeason + currentEpisode).offsetTop;
+        dropdownContent.scrollTop = topPos - 21.37 * 4;
+    }
+
+
     dropdownLabel.appendChild(dropdownLabelText);
     breadcrumbName.replaceChildren(dropdownLabel);
+    dropdown.appendChild(dropdownButton);
+    dropdown.appendChild(dropdownContent);
     breadcrumbName.appendChild(dropdown);
+
+
+    // add cursor change when hover on dropdown items
+    var css = 'button#dropdown-button:hover, div#dropdown-content a:hover { background-color: #010103 !important; color: #9090A0 !important; }',
+        head = document.head || document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
+
+    head.appendChild(style);
+
+    style.type = 'text/css';
+    if (style.styleSheet) {
+        // This is required for IE8 and below.
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+
+
+    const breadcrumb = document.querySelector(".breadcrumb");
+    breadcrumb.style.justifyContent = "center";
+    breadcrumb.style.alignItems = "center";
+    breadcrumbName.style.display = "flex";
+    breadcrumbName.style.justifyContent = "center";
+    breadcrumbName.style.alignItems = "center";
 
     // fetching series main page containing all seasons info
     let main_series_page_request = new XMLHttpRequest();
@@ -196,34 +306,46 @@ function main() {
                                         ...
                                     <optgroup/>
                                 */
-                const sOptGroup = document.createElement("optgroup");
-                sOptGroup.label = "Season " + season.id;
+                const sOptGroup = document.createElement("div");
+                sOptGroup.id = "season" + season.id;
+                sOptGroup.style.textAlign = "center";
+
+                const seasonLabel = document.createElement("b");
+                seasonLabel.style.paddingTop = "10px";
+                seasonLabel.style.display = "inline-block";
+                seasonLabel.style.color = "#757585";
+
+                seasonLabel.appendChild(document.createTextNode("Season " + season.id));
+                sOptGroup.appendChild(seasonLabel);
                 // create option for episodes
                 season.episodes.forEach((episode) => {
                     const epName = Number(episode.id) + " " + episode.name;
-                    const epOption = document.createElement("option");
+                    const epLink = document.createElement("a");
+                    epLink.style.display = "block";
+                    epLink.style.padding = "2px 8px";
+                    epLink.style.textAlign = "left";
 
-                    epOption.value = season.id + episode.id; // to distinguish all episodes form different seasons
+                    epLink.id = season.id + episode.id; // to distinguish all episodes form different seasons
 
                     // complete link
                     const link =
                         linkWithoutSeasonAndEpisode + `S${season.id}E${episode.id}`;
-                    epOption.onclick = () => (window.location.href = link);
+                    epLink.href = link;
 
                     const epText = document.createTextNode(epName);
 
-                    epOption.appendChild(epText);
-                    sOptGroup.appendChild(epOption);
+                    epLink.appendChild(epText);
+                    sOptGroup.appendChild(epLink);
                 });
-                dropdown.appendChild(sOptGroup);
+                dropdownContent.appendChild(sOptGroup);
             });
 
-            dropdown.value = currentSeason + currentEpisode;
+            const cuEp = seasons[Number(currentSeason) - 1].episodes[Number(currentEpisode) - 1];
+            dropdownButton.appendChild(document.createTextNode(cuEp.id + " " + cuEp.name));
             // style current episode so it stands out
-            const currentOption = dropdown.querySelector(
-                `option[value="${currentSeason + currentEpisode}"]`
-            );
-            currentOption.style.color = "#ffffff";
+            const currentOption = dropdown.querySelector(`a[id="${ currentSeason + currentEpisode}"]`);
+            currentOption.style.color = "#B0B0D0";
+            currentOption.style.backgroundColor = "#212123";
         } catch (e) {
             // I used it for debug, but I guess it could stay
             alert("uflix-nex-button error: " + e);
